bd3de2bec432bda2b855eafd31e53ad020320a4f
johndelanetaylor <johndelanetaylor@gmail.com>
2015-05-06 11:32:24 -0400

    updated background, type styles, and layout

---
+++ js/app.js
@@ -0,0 +1,11 @@
+/* <button type="button" onclick="document.getElementById('hooray').style.color = '#0000FF'">Submit</button> */
+<script>
+
+    document.getElementById('confetti').onclick = changeColor;   
+
+    function changeColor() {
+				document.h1.getElementById(id).style.color = "#0000FF"; // forecolor
+        return false;
+    }   
+
+</script>
